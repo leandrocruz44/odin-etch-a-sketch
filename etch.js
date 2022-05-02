6b15d74b2b1781ea9ad752384945ca9053eda612
leandrocruz <leandro.dinamo@gmail.com>
2022-05-02 14:37:35 -0300

    Change paint color, rework resize button
    *The prompt on the Resize button now asks again the value the user wants
    until a valid number is entered, instead of asking to click the Resize
    button again.
    *The sketch color is now brown, applied under a variable.
    *CSS tweaks:
      *border-radius of the main container
      *h1 font-weight is lighter
      *h1 margin reduced
      *Resize and Clear button now have a hover effect

--- etch.js
+++ etch.js
@@ -1,8 +1,7 @@
 const container = document.querySelector('#container');
 const initialSize = 16;
-const r = 0;
-const g = 255;
-const b = 200;
+const color = 'brown';
+
 
 function squareSize(initialSize) {
     const square = 450/initialSize;
@@ -13,58 +12,61 @@ function squareSize(initialSize) {
 function createSquare(initialSize) {
     for(let i = 0; i < initialSize ** 2; i++) {
         const square = document.createElement('div');
-        const size = squareSize(initialSize)
-        square.classList.toggle('square')
-        square.style.cssText = `width: ${size}px; height: ${size}px`
+        const size = squareSize(initialSize);
+        square.classList.toggle('square');
+        square.style.cssText = `width: ${size}px; height: ${size}px`;
         container.append(square);
     }
 }
 
-function paintSquare(initialSize) {
-    const size = squareSize(initialSize)
-    const color = 'black'
+function paintSquare(initialSize, color) {
+    const size = squareSize(initialSize);
+    const paint = color;
     const squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
-        square.addEventListener('mouseover', () => {
-            square.style.cssText = `background-color: ${color}; width: ${size}px; height: ${size}px`
+        square.addEventListener('mouseout', () => {
+            square.style.cssText = `background-color: ${paint}; width: ${size}px; height: ${size}px`
         })
     })
 }
 
 
-function resizeSquares() {
+function resizeSquares(color) {
+    const paint = color
     const button = document.querySelector('#btnResize');
     let div = document.getElementById('container');
     button.addEventListener('click', () => {
         while(div.firstChild) {
             div.removeChild(div.firstChild);
         }
-        let ask = prompt('How many squares do you want? Choose between 2 and 100.')
-        if (ask < 2 || ask > 100) {
-            alert('Invalid input.\n\nInsert numbers only between 2 and 100. Click "Resize" to set new value.')
-        } else {
-            squareSize(ask);
-            createSquare(ask);
-            paintSquare(ask);
-            clearAll(ask);
+        let ask = '';
+        while (ask < 2 || ask > 100) {
+            ask = prompt('What grid size do you want? \n\nChoose between 2 and 100.');
+            if (ask >= 2 && ask <= 100) {
+                squareSize(ask);
+                createSquare(ask);
+                paintSquare(ask, paint);
+                clearAll(ask);
+                break;
+            }
         }
     })
 }
 
 function clearAll(number) {
-    const size = squareSize(number)
+    const size = squareSize(number);
     const button = document.querySelector('#btnClear');
     const squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
         button.addEventListener('click', () => {
-            square.style.cssText = `background-color: rgb(255, 255, 203); width: ${size}px; height: ${size}px`
+            square.style.cssText = `background-color: rgb(255, 255, 203); width: ${size}px; height: ${size}px`;
         })
     })
 }
 
 
-squareSize(initialSize)
+squareSize(initialSize);
 createSquare(initialSize);
-paintSquare(initialSize);
+paintSquare(initialSize, color);
 clearAll(initialSize);
-resizeSquares();
+resizeSquares(color);
