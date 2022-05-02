c043727f85a2733a8ded7ef5cac2c6162ee70ba1
leandrocruz <leandro.dinamo@gmail.com>
2022-05-02 02:08:00 -0300

    Delete slider, create function resizeSquares
    Add function resizeSquares to set a value for how much squares the user
    wants to play with. Use CSS to stylize the page with pastel colors that
    remind the 80's aesthetic.

--- etch.js
+++ etch.js
@@ -1,32 +1,53 @@
 const container = document.querySelector('#container');
-const howMany = 16;
-const r = 200;
-const g = 32;
-const b = 100;
+const initialSize = 16;
+const r = 0;
+const g = 255;
+const b = 200;
 
-function squareSize(size) {
-    const square = 500/size;
+function squareSize(initialSize) {
+    const square = 450/initialSize;
     return square;
 }
 
-function createSquare(number) {
-    for(let i = 0; i < number * number; i++) {
+
+function createSquare(initialSize) {
+    for(let i = 0; i < initialSize ** 2; i++) {
         const square = document.createElement('div');
-        const size = squareSize(number)
+        const size = squareSize(initialSize)
         square.classList.toggle('square')
         square.style.cssText = `width: ${size}px; height: ${size}px`
         container.append(square);
     }
 }
 
-
-function paintSquare(number, r, g, b) {
-    const size = squareSize(number)
+function paintSquare(initialSize) {
+    const size = squareSize(initialSize)
+    const color = 'black'
     const squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
         square.addEventListener('mouseover', () => {
-            square.style.cssText = `background-color: rgb(${r}, ${g}, ${b}); width: ${size}px; height: ${size}px`
+            square.style.cssText = `background-color: ${color}; width: ${size}px; height: ${size}px`
         })
+    })
+}
+
+
+function resizeSquares() {
+    const button = document.querySelector('#btnResize');
+    let div = document.getElementById('container');
+    button.addEventListener('click', () => {
+        while(div.firstChild) {
+            div.removeChild(div.firstChild);
+        }
+        let ask = prompt('How many squares do you want? Choose between 2 and 100.')
+        if (ask < 2 || ask > 100) {
+            alert('Invalid input.\n\nInsert numbers only between 2 and 100. Click "Resize" to set new value.')
+        } else {
+            squareSize(ask);
+            createSquare(ask);
+            paintSquare(ask);
+            clearAll(ask);
+        }
     })
 }
 
@@ -36,11 +57,14 @@ function clearAll(number) {
     const squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
         button.addEventListener('click', () => {
-            square.style.cssText = `background-color: rgb(255, 255, 255); width: ${size}px; height: ${size}px`
+            square.style.cssText = `background-color: rgb(255, 255, 203); width: ${size}px; height: ${size}px`
         })
     })
 }
 
-createSquare(howMany);
-paintSquare(howMany, r, g, b);
-clearAll(howMany);
+
+squareSize(initialSize)
+createSquare(initialSize);
+paintSquare(initialSize);
+clearAll(initialSize);
+resizeSquares();
